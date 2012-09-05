e41de204b867cb0def8f5f571539ce2055d3438e
benjaoming <benjaoming@gmail.com>
2012-09-05 02:08:26 +0200

    Fixing setup.py file to work with pypi and adding MANIFEST.in to assist template files and README

--- setup.py
+++ setup.py
@@ -14,14 +14,32 @@ CLASSIFIERS = [
     'Topic :: Software Development :: Libraries :: Application Frameworks',
 ]
 
+packages = find_packages()
+
+def build_media_pattern(base_folder, file_extension):
+    return ["%s/%s*.%s" % (base_folder, "*/"*x, file_extension) if base_folder else "%s*.%s" % ("*/"*x, file_extension) for x in range(10)]
+
+media_patterns = ( build_media_pattern("templates", "html") +
+     	           build_media_pattern("static", "js") +
+                   build_media_pattern("static", "css") +
+                   build_media_pattern("static", "png") +
+                   build_media_pattern("static", "jpeg") +
+                   build_media_pattern("static", "gif") +
+                   build_media_pattern("", "rst")
+)
+
+package_data = dict(
+    (package_name, media_patterns)
+    for package_name in packages
+)
 
 setup(name='stopforumspam',
-      description='Django middleware for blocking IPs from stopforumspam.com',
+      description='Django middleware for blocking IPs listed in stopforumspam.com',
       long_description=open(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
       author="Benjamin Bach",
       author_email="benjamin@overtag.dk",
-      version='1.0',
-      packages=find_packages(),
+      version='1.1',
+      packages=packages,
       license='BSD License',
       install_requires=[
         'Django>=1.2.0',
@@ -29,4 +47,5 @@ setup(name='stopforumspam',
       url='https://overtag.dk/',
       classifiers=CLASSIFIERS,
       include_package_data=True,
-      )
+      package_data=package_data,
+)
