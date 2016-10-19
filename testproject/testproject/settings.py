2b74f1f412d0291016f88e5cb783fb75e1f33d06
Benjamin Bach <benjaoming@gmail.com>
2016-10-19 14:46:28 +0200

    Add a couple of more known settings to testpro

--- testproject/testproject/settings.py
+++ testproject/testproject/settings.py
@@ -14,14 +14,7 @@ DATABASES = {
         # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'ENGINE': 'django.db.backends.sqlite3',
         # Or path to database file if using sqlite3.
-        'NAME': 'db/test.db',
-        # The following settings are not used with sqlite3:
-        'USER': '',
-        'PASSWORD': '',
-        # Empty for localhost through domain sockets or '127.0.0.1' for
-        # localhost through TCP.
-        'HOST': '',
-        'PORT': '',                      # Set to empty string for default.
+        'NAME': 'test.db',
     }
 }
 
@@ -131,4 +124,11 @@ INSTALLED_APPS = (
     # 'django.contrib.admindocs',
 )
 
+SFS_ALL_POST_REQUESTS = True
+
+# SFS_SOURCE_ZIP = "http://www.stopforumspam.com/downloads/listed_ip_7.zip"
+# SFS_SOURCE_ZIP = "file:///tmp/listed_ip_7.zip"
 SFS_SOURCE_ZIP = "file://listed_ip_7.zip"
+
+SFS_ZIP_FILENAME = "listed_ip_7.txt"
+SFS_CACHE_EXPIRE = 1  # day
