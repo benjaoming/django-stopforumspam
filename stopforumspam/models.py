cc8881fa53a96b395b335ac4ddad0c81be6d13f3
Matthew Newton <matthewn@berkeley.edu>
2022-03-20 15:49:19 -0700

    make compatible with Django 4.0

--- stopforumspam/models.py
+++ stopforumspam/models.py
@@ -2,7 +2,7 @@ from datetime import timedelta
 
 from django.db import models
 from django.utils import timezone
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 
 from . import settings as sfs_settings
